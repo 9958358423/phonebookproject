c71c8f5bf70b342da897e2095ef83bb1ec835ba7
unknown <goelritik31@gmail.com>
2020-05-28 17:26:32 +0530

    changes

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var express = require("express")
 var app = express()
 var bodyParser = require("body-parser")
+var mongoose=require("mongoose")
+var methodOverride=require("method-override")
+
+mongoose.connect("mongodb://localhost/Phonebook",{
+useUnifiedTopology:true,
+useNewUrlParser:true,
+useCreateIndex:true
+}).then(() =>console.log("DB Connected!"))
+.catch(err =>{
+    console.log("DB Connection Error : $(err.message)");
+});
+
 app.set("view engine","ejs")
 app.set("port",process.env.PORT||8080)
 console.log("server is running")
